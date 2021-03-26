03d26d773736b9d06f7860e09c240bd7ab755388
gayfulina <irinmdr@gmail.com>
2021-03-26 13:47:16 -0700

    homepage redesign6

--- src/pages/pages/homePage/HomePage.tsx
+++ src/pages/pages/homePage/HomePage.tsx
@@ -13,55 +13,66 @@ function HomePage(props: IProps) {
   return (
     <>
       <section id="product-overview">
-          <h1 className="pt-5">DISCOVER YOGA
-             <br />
-            THE JOURNEY STARTS HERE
-          </h1>
+        <h1 className="pt-5">
+          DISCOVER YOGA
+          <br />
+          THE JOURNEY STARTS HERE
+        </h1>
 
-          <div className="container">
-
-            <div className="row d-flex justify-content-center">
-
-              <div className="col-lg-3">
-                <h2 className="m-2">education</h2>
-              </div>
-
-              <div className="col-lg-3">
-                <h2 className="m-2">information</h2>
-              </div>
-
-              <div className="col-lg-3">
-                <h2 className="m-2">community</h2>
-              </div>
-
+        <div className="container">
+          <div className="row d-flex justify-content-center">
+            <div className="col-lg-3">
+              <h2 className="m-2">education</h2>
             </div>
 
-            {!isUserAuth && (
-              <div className="row d-flex justify-content-center">
+            <div className="col-lg-3">
+              <h2 className="m-2">information</h2>
+            </div>
 
-                <div className="col-lg-5 my-lg-4 my-2 d-flex justify-content-lg-start justify-content-center">
-                  <a href="/user/register" className="button-home m-2">
-                    I‘M A TEACHER
-                  </a>
-                </div>
-
-                <div className="col-lg-5 my-lg-4 d-flex justify-content-lg-end justify-content-center">
-                  <a href="/user/register" className="button-home m-2">
-                    I’M A STUDENT
-                  </a>
-                </div>
-
-              </div>
-            )}
+            <div className="col-lg-3">
+              <h2 className="m-2">community</h2>
+            </div>
           </div>
 
+          {!isUserAuth && (
+            <div className="row d-flex justify-content-center">
+              <div className="col-lg-4 my-lg-4 my-2 d-flex justify-content-lg-start justify-content-center">
+                <a href="/user/register" className="button-home m-2">
+                  I‘M A TEACHER
+                </a>
+              </div>
+
+              <div className="col-lg-4 my-lg-4 d-flex justify-content-lg-end justify-content-center">
+                <a href="/user/register" className="button-home m-2">
+                  I’M A STUDENT
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
       </section>
 
       <section id="product-description" className="container mt-5 mb-5 mx-auto">
+
         <div className="row d-flex justify-content-center">
-          <div className="col-lg-3 p-2"> </div>
-          <div className="col-lg-3 p-2"> </div>
-          <div className="col-lg-3 p-2"> </div>
+
+          <div className="col-lg-3 d-flex justify-content-center">
+            <a href="" className="button-home m-2">
+              FIND YOUR TEACHER
+            </a>
+          </div>
+
+          <div className="col-lg-3 d-flex justify-content-center">
+            <a href="" className="button-home m-2">
+            FIND YOUR STUDIO
+          </a>
+          </div>
+
+          <div className="col-lg-3 d-flex justify-content-center">
+            <a href="" className="button-home m-2 ">
+              FIND YOUR RETREAT
+            </a>
+          </div>
         </div>
       </section>
     </>
