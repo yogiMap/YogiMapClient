f02d7fcbccdbf7699d8aaf411fd69834e6b2a4a2
gayfulina <irinmdr@gmail.com>
2021-03-29 15:44:35 -0700

    profile

--- src/pages/user/profile/UserProfile.tsx
+++ src/pages/user/profile/UserProfile.tsx
@@ -7,8 +7,9 @@ import UserRoles from '@/pages/user/profile/UserRoles';
 const UserProfile = (props: any) => {
   const userId = get(props, 'match.params.userId', '');
 
-  const userInfo = get(props, 'userInfo', '');
-  const userName = get(userInfo, 'name', '');
+  const userInfo = get(props, 'userInfo.payload', '');
+  const userName = get(userInfo, 'firstName', '');
+  const name = get(userInfo, 'name', '');
   const roles = get(userInfo, 'roles', []);
   const about = get(userInfo, 'about', '');
   const email = get(userInfo, 'email', '');
@@ -18,18 +19,106 @@ const UserProfile = (props: any) => {
   }, []);
 
   return (
-    <div>
+    <div className="profile-bg">
       <h1>Profile {userName}</h1>
-      <Row>{!isEmpty(roles) ? <UserRoles roles={roles} /> : null}</Row>
+      <div className="container">
+        <div className="row d-flex justify-content-center">
+          <div className="col-lg-9">
+            <p className="profile-greetings text-center">
+              Welcome {userName}! Yogimap empowers yoga teachers to manage & grow their business - let`s get you set up:
+            </p>
+          </div>
+        </div>
 
-      <Row>
-        {about ? (
-          <Col span={12}>
-            <h3>About</h3>
-            <div>{about}</div>
-          </Col>
-        ) : null}
-      </Row>
+        <div className="row d-flex justify-content-center">
+          <div className="col-lg-3">
+            <div className="profile-side-panel">
+              <h5> {name} </h5>
+              <h6> Teacher of your </h6>
+              <h6> Edit Profile </h6>
+              <hr />
+              <p>
+                Picture <br />
+                Styles of Yoga and Lineages
+                <br />
+                Description
+                <br />
+                Location
+                <br />
+                Contacts
+                <br />
+                YouTube
+                <br />
+                Instagram
+                <br />
+                Facebook
+                <br />
+                Reset Password
+                <br />
+              </p>
+            </div>
+          </div>
+
+          <div className="col-lg-6">
+            <div className="profile-main">
+              Picture
+              <button className="button-primary float-end">Save</button>
+            </div>
+
+            <div className="profile-main">
+              Styles of Yoga and Lineages
+              <button className="button-primary float-end">Save</button>
+            </div>
+
+            <div className="profile-main">
+              Description
+              <button className="button-primary float-end">Save</button>
+            </div>
+
+            <div className="profile-main">
+              Location
+              <button className="button-primary float-end">Save</button>
+            </div>
+
+            <div className="profile-main">
+              Contacts
+              <button className="button-primary float-end">Save</button>
+            </div>
+
+            <div className="profile-main">
+              YouTube
+              <button className="button-primary float-end">Save</button>
+            </div>
+
+            <div className="profile-main">
+              Instagram
+              <button className="button-primary float-end">Save</button>
+            </div>
+
+            <div className="profile-main">
+              Facebook
+              <button className="button-primary float-end">Save</button>
+            </div>
+
+            <div className="profile-main">
+              Reset Password
+              <button className="button-primary float-end">Save</button>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      {/*<Row>{!isEmpty(roles) ? <UserRoles roles={roles} /> : null}</Row>*/}
+
+      {/*<Row>*/}
+      {/*  {about ? (*/}
+      {/*    <Col span={12}>*/}
+      {/*      <h3>About</h3>*/}
+      {/*      <div>{about}</div>*/}
+      {/*    </Col>*/}
+      {/*  ) : null}*/}
+      {/*</Row>*/}
     </div>
   );
 };
