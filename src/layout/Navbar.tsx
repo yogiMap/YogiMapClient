7b148c56ae94311a2c4136954f592002a02697fa
gayfulina <irinmdr@gmail.com>
2021-04-09 15:16:34 -0700

    navbar toggler

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,32 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import Logo from '../images/logo_512x512.png';
 import { Link } from 'umi';
+
+const { JSDOM } = require('jsdom');
+const { window } = new JSDOM('');
+const $ = require('jquery')(window);
+
 import AdminMenu from '@/layout/_menu/AdminMenu';
 import TopMenu from '@/layout/_menu/TopMenu';
 import UserInfo from '@/pages/user/topInfo/UserInfo';
 
 const Navbar = () => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
-    <nav className="navbar navbar-expand-lg bg-light-">
-      <div className="container-fluid">
-        <Link to="/" className="navbar-brand">
+    <nav className='navbar navbar-expand-lg navbar-light bg-light-'>
+      <div className='container-fluid'>
+        <Link to='/' className='navbar-brand'>
           YogiMap
         </Link>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
+        <button className='custom-toggler navbar-toggler'
+                type='button'
+                data-toggle='collapse'
+                data-target='#navbarsExample09'
+                aria-controls='navbarsExample09'
+                aria-expanded={!isNavCollapsed ? true : false}
+                aria-label='Toggle navigation'
+                onClick={handleNavCollapse}>
+          <span className='navbar-toggler-icon'></span>
         </button>
 
-        <AdminMenu />
-        <TopMenu />
-        <UserInfo />
+        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id='navbarsExample09'>
+          <div className='d-flex justify-content-end'>
+            <UserInfo />
+            <TopMenu />
+            <AdminMenu />
+          </div>
+        </div>
+
       </div>
     </nav>
   );
