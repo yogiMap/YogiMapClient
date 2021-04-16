4c872c39b8d2edd39784e32d820e11640b0ff7e1
gayfulina <irinmdr@gmail.com>
2021-04-16 16:15:50 -0700

    styles reformat4

--- src/pages/pages/homePage/HomePage.tsx
+++ src/pages/pages/homePage/HomePage.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { connect, Link } from 'umi';
 import { get } from 'lodash';
 import { IUserAccount } from '@/pages/user/userSearch/types';
+import body from '@/pages/pages/homePage/images/yogigirl.png';
+import mind from '@/pages/pages/homePage/images/crystals.jpeg';
+import soul from '@/pages/pages/homePage/images/monks.jpg';
 
 interface IProps {
   Account: IUserAccount;
@@ -13,8 +16,76 @@ function HomePage(props: IProps) {
   return (
     <>
       <div className="home-bg">
-        <h1 className="pt-5">DISCOVER YOGA</h1>
-        <h1 className="my-4">THE JOURNEY STARTS HERE</h1>
+        <div className="home-bg-headers">
+          <h1 className="pt-5">DISCOVER YOGA</h1>
+          <h1 className="my-4">THE JOURNEY STARTS HERE</h1>
+        </div>
+      </div>
+
+      <div className="home-stripe-under-bg">
+        <div className="container">
+          {!isUserAuth && (
+            <div className="row d-flex justify-content-center">
+              <div className="col-lg-4 my-lg-5 my-2 d-flex justify-content-lg-start justify-content-center">
+                <Link to="/user/register/teacher" className="home-button m-2">
+                  I‘M A TEACHER
+                </Link>
+              </div>
+
+              <div className="col-lg-4 my-lg-5 d-flex justify-content-lg-end justify-content-center">
+                <Link to="/user/register" className="home-button m-2">
+                  I’M A STUDENT
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="home-description">
+        <div className="home-bg-header-secondary">
+          <h2>FIND YOUR GURU</h2>
+        </div>
+      </div>
+
+      <div className="home-yoga-type container m-5">
+        <div className="row d-flex justify-content-center">
+
+          <div className="col-lg-3 p-5">
+            <div className="img-container">
+              <img src={body} alt="body" />
+
+              <div className="home-stripe-body"></div>
+              <Link to="" className="button-body-mind-soul">
+                BODY
+              </Link>
+
+            </div>
+            </div>
+
+
+          <div className="col-lg-3 p-5">
+            <div className="img-container">
+              <img src={mind} alt="mind" />
+
+              <div className="home-stripe-mind"></div>
+              <Link to="" className="button-body-mind-soul">
+                MIND
+              </Link>
+            </div>
+          </div>
+
+          <div className="col-lg-3 p-5">
+            <div className="img-container">
+              <img src={soul} alt="soul" />
+
+              <div className="home-stripe-soul"></div>
+              <Link to="" className="button-body-mind-soul">
+                SOUL
+              </Link>
+            </div>
+          </div>
+        </div>
 
         <div className="container">
           <div className="row d-flex justify-content-center">
@@ -29,50 +100,6 @@ function HomePage(props: IProps) {
             <div className="col-lg-3">
               <h2 className="m-3">community</h2>
             </div>
-          </div>
-
-          {!isUserAuth && (
-            <div className="row d-flex justify-content-center">
-              <div className="col-lg-4 my-lg-5 my-2 d-flex justify-content-lg-start justify-content-center">
-                <a href="/user/register/teacher" className="home-button m-2">
-                  I‘M A TEACHER
-                </a>
-              </div>
-
-              <div className="col-lg-4 my-lg-5 d-flex justify-content-lg-end justify-content-center">
-                <a href="/user/register" className="home-button m-2">
-                  I’M A STUDENT
-                </a>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-
-      <div className="home-description p-5 mb-5">
-
-        <h4 className="pt-5 pb-3 px-5">find your yoga guru</h4>
-
-        <div className="container mb-5 mx-auto">
-          <div className="row d-flex justify-content-center">
-            <div className="col-lg-3 d-flex justify-content-center">
-              <div className="home-description__image m-2">
-                <Link to="" className="home-description-button">MIND</Link>
-            </div>
-            </div>
-
-            <div className="col-lg-3 d-flex justify-content-center">
-              <div className="home-description__image m-2">
-              <Link to="" className="home-description-button">BODY</Link>
-            </div>
-            </div>
-
-            <div className="col-lg-3 d-flex justify-content-center">
-              <div className="home-description__image m-2">
-                <Link to="" className="home-description-button">SOUL</Link>
-              </div>
-            </div>
-
           </div>
         </div>
       </div>
