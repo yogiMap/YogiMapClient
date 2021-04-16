9e67271d384387de2010d49f092696ca267d4310
gayfulina <irinmdr@gmail.com>
2021-04-15 17:26:30 -0700

    navbar2

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -12,58 +12,56 @@ const Navbar = () => {
   // const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
   return (
-    <nav className="navbar navbar-expand-xl">
-      <div className="container flex-nowrap">
-        <Link to="/" className="site-name text-nowrap">
-          <img src={logo} alt="Yogi Map Logo" height={40} className="logo mr-2" />
-        </Link>
-
-        <div className="navbar-toggler">
-          <div className="d-flex align-items-center">
-            <TopMenuDropdown />
-            <UserInfoNavCollapse />
-          </div>
-        </div>
-
-        <div className="collapse navbar-collapse">
-          <div className="navbar-nav">
-            <TopMenu />
-            <AdminMenu />
-          </div>
-
-          <UserInfo />
-        </div>
-      </div>
-    </nav>
-
-
-    // <nav className='navbar navbar-expand-lg navbar-light bg-light-'>
-    //   <div className='container-fluid'>
-    //     <Link to='/' className='navbar-brand'>
-    //       YogiMap
+    // <nav className="navbar navbar-expand-xl">
+    //   <div className="container flex-nowrap">
+    //     <Link to="/" className="site-name text-nowrap">
+    //       <img src={logo} alt="Yogi Map Logo" height={40} className="logo mr-2" />
     //     </Link>
     //
-    //     <button className='custom-toggler navbar-toggler'
-    //             type='button'
-    //             data-toggle='collapse'
-    //             data-target='#navbarsExample09'
-    //             aria-controls='navbarsExample09'
-    //             aria-expanded={!isNavCollapsed ? true : false}
-    //             aria-label='Toggle navigation'
-    //             onClick={handleNavCollapse}>
-    //       <span className='navbar-toggler-icon'></span>
-    //     </button>
-    //
-    //     <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id='navbarsExample09'>
-    //       <div className='d-flex justify-content-end'>
-    //         <AdminMenu />
-    //         <TopMenu />
+    //     <div className="navbar-toggler">
+    //       <div className="d-flex align-items-center">
     //         <UserInfo />
+    //         <TopMenuDropdown />
+    //         <UserInfoNavCollapse />
     //       </div>
     //     </div>
     //
+    //     <div className="collapse navbar-collapse">
+    //       <div className="navbar-nav">
+    //         <TopMenu />
+    //         <AdminMenu />
+    //       </div>
+    //
+    //
+    //     </div>
     //   </div>
     // </nav>
+
+
+    <nav className='navbar navbar-expand-lg navbar-light bg-light-'>
+      <div className='container-fluid'>
+           <Link to="/" className="site-name text-nowrap">
+           <img src={logo} alt="Yogi Map Logo" height={40} className="logo mr-2" />
+          </Link>
+
+        {/*<button className='custom-toggler navbar-toggler'*/}
+        {/*        type='button'*/}
+        {/*        data-toggle='collapse'*/}
+        {/*        data-target='#navbarsExample09'*/}
+        {/*        aria-controls='navbarsExample09'*/}
+        {/*        aria-expanded={!isNavCollapsed ? true : false}*/}
+        {/*        aria-label='Toggle navigation'*/}
+        {/*        onClick={handleNavCollapse}>*/}
+        {/*  <span className='navbar-toggler-icon'></span>*/}
+        {/*</button>*/}
+
+          <div className='d-flex justify-content-end'>
+            <AdminMenu />
+            <TopMenu />
+            <UserInfo />
+          </div>
+      </div>
+    </nav>
   );
 };
 
