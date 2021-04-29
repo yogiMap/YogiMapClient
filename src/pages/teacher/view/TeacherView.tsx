17f3fb7e901f58dfb11819a9a0de4f8c09a13f37
gayfulina <irinmdr@gmail.com>
2021-04-28 17:23:46 -0700

    teacher3

--- src/pages/teacher/view/TeacherView.tsx
+++ src/pages/teacher/view/TeacherView.tsx
@@ -3,6 +3,7 @@ import { connect } from 'umi';
 import { get } from 'lodash';
 import TeacherViewClassesList from '@/pages/teacher/view/TeacherViewClassesList';
 import TeacherViewEventList from '@/pages/teacher/view/TeacherViewEventList';
+import { Tag } from 'antd';
 
 interface IProps {
   teacherId: string;
@@ -20,6 +21,17 @@ const TeacherView = (props: IProps) => {
   const teacherTypeObject = get(props, 'TeacherView.teacherType', {});
   const teacherType = Object.values(teacherTypeObject);
   // const classes = Object.keys(classesObject).map(el => ({name:el, value: classesObject[el]}) )
+  const addressId: string = get(props, 'match.params.addressId');
+  const addressLine1: string = get(props, 'TeacherView.addressLine1', '');
+  const addressLine2: string = get(props, 'TeacherView.addressLine2', '');
+  const state: string = get(props, 'TeacherView.state', '');
+  const city: string = get(props, 'TeacherView.city', '');
+  const zipCode: string = get(props, 'TeacherView.zipCode', '');
+  const countryName: string = get(props, 'TeacherView.countryName', '');
+  const isDefault: boolean = get(props, 'TeacherView.isDefault', false);
+  const timeZone = get(props, 'TeacherView.timeZone', '');
+  const email = get(props, 'TeacherView.email', '');
+  const phone: any = get(props, 'TeacherView.phone', '');
 
   console.log(event);
 
@@ -29,12 +41,24 @@ const TeacherView = (props: IProps) => {
 
   return (
     <div className="container">
-      <h1>{name}</h1>
-      <h3>Teacher Type</h3>
+      <h1 className="text-center">{name}</h1>
       {teacherType.map((el: any) => (
-        <div>{el.name}</div>
+        <h2>{el.name}</h2>
       ))}
 
+      <div className="d-print-block">
+        <h6>
+          Address {isDefault && <Tag color="default">default</Tag>}
+        </h6>
+        <div className="mb-1">{addressLine1}</div>
+        <div className="mb-1">{addressLine2}</div>
+        <div className="mb-1">{city}</div>
+        <div className="mb-1">{state}</div>
+        <div className="mb-1">{zipCode}</div>
+        <div className="mb-1">{countryName}</div>
+        <div className="mb-1">{email}</div>
+        <div className="mb-1">{phone}</div>
+      </div>
 
       <h3 className="mt-5">Classes</h3>
       <div>
