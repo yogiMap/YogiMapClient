9ccbf2d4a4fb17c9451d2a76cf2641dc483220bc
gayfulina <irinmdr@gmail.com>
2021-05-04 17:14:15 -0700

    teacherAccount6

--- src/pages/teacherAccount/view/TeacherAccountView.tsx
+++ src/pages/teacherAccount/view/TeacherAccountView.tsx
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import { connect } from 'umi';
 import { get } from 'lodash';
 import RenderPhoneNumber from '@/pages/phoneNumberRendering/PhoneNumbersRendering';
+import { Tag } from 'antd';
+import TeacherAccountViewClassesList from '@/pages/teacherAccount/view/TeacherAccountViewClassesList';
+import TeacherAccountViewEventList from '@/pages/teacherAccount/view/TeacherAccountViewEventList';
 
 interface IProps {
   teacherAccountId: string;
@@ -19,15 +22,64 @@ const TeacherAccountView = (props: IProps) => {
   const city = get(props, 'TeacherAccountView.city', '');
   const state = get(props, 'TeacherAccountView.state', '');
   const country = get(props, 'TeacherAccountView.country', '');
+  const countryName: string = get(props, 'TeacherAccountView.countryName', '');
   const zipCode = get(props, 'TeacherAccountView.zipCode', '');
   const timeZone = get(props, 'TeacherAccountView.timeZone', '');
+  const isDefault: boolean = get(props, 'TeacherAccountView.isDefault', false);
+  const phone: any = get(props, 'TeacherAccountView.phone', '');
+
+  const name = get(props, 'TeacherAccountView.name', '');
+  const classesObject = get(props, 'TeacherAccountView.classes', {});
+  const classes: any  = Object.values(classesObject);
+  const eventObject = get(props, 'TeacherAccountView.event', {});
+  const event: any  = Object.values(eventObject);
+  const classTypeObject = get(props, 'TeacherAccountView.classType', {});
+  const focusObject = get(props, 'TeacherAccountView.focus', {});
+  const YogaStyleObject = get(props, 'TeacherAccountView.style', {});
+  console.log(event);
+
+
 
   useEffect(() => {
     props.teacherAccountGetById(teacherAccountId);
   }, []);
 
+  // @ts-ignore
   return (
     <div>
+
+      <div className="container">
+        <h1 className="text-center">{name}</h1>
+        { focusObject.map((el: any) => (
+          <h2>{el.name}</h2>
+        ))}
+
+        <div className="d-print-block">
+          <h6>
+            Address {isDefault && <Tag color="default">default</Tag>}
+          </h6>
+          <div className="mb-1">{addressLine1}</div>
+          <div className="mb-1">{addressLine2}</div>
+          <div className="mb-1">{city}</div>
+          <div className="mb-1">{state}</div>
+          <div className="mb-1">{zipCode}</div>
+          <div className="mb-1">{countryName}</div>
+          <div className="mb-1">{email}</div>
+          <div className="mb-1">{phone}</div>
+        </div>
+
+        <h3 className="mt-5">Classes</h3>
+        <div>
+          <TeacherAccountViewClassesList classes={classes}/>
+        </div>
+
+        <h3 className="mt-5">Events</h3>
+        <div>
+          <TeacherAccountViewEventList event={event}/>
+        </div>
+
+      </div>
+
       <h2>{teacherName}</h2>
       <hr />
       Phone Number: <RenderPhoneNumber phoneNumberAll={get(props, 'TeacherAccountView.phoneNumber', {})} />
---
+++ src/pages/teacherAccount/view/TeacherAccountViewClassesList.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { get } from 'lodash';
+import { Button, Table } from 'antd';
+import { ColumnProps } from 'antd/es/table';
+import { Link, withRouter } from 'umi';
+import { RouteComponentProps } from 'react-router-dom';
+import ActionMenu from '@/pages/teacher/dashboard/search/ActionMenu';
+import { IClasses } from '@/pages/classes/types';
+import ClassesDashboardControls from '@/pages/classes/dashboard/controls/ClassesDashboardControls';
+import { ISidepanel } from '@/pages/utils/sidepanel/types';
+import { IUserAccount } from '@/pages/user/userSearch/types';
+
+interface IProps extends RouteComponentProps {
+  classes: IClasses[];
+  open: (arg: ISidepanel) => void;
+  Account: IUserAccount;
+}
+
+const TeacherAccountViewClassesList = (props: IProps) => {
+  const queryParams = get(props, 'location.query', {});
+  const classes: any  = get(props, 'classes', []);
+  const isUserAuth = get(props, 'Account._id');
+
+  const columns: ColumnProps<IClasses>[] = [
+    {
+      title: 'Name',
+      key: 'name',
+      dataIndex: 'name',
+      // render: (row) => {
+      //   return <Link to={`/classes/${get(row, 'classes._id')}`}>{get(row, 'classes.name')}</Link>;
+      // },
+      // render: (row) => <Link to={`/classes/${row._id}`}>{row.name}</Link>,
+    },
+    {
+      title: 'YogaStyle',
+      key: 'yogaStyle',
+      dataIndex: 'yogaStyle',
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      className: 'actions',
+      width: 80,
+      render: (row) => <ActionMenu row={row} queryParams={queryParams} />,
+    },
+  ];
+
+  return (
+    <div>
+      <Table
+        rowKey="_id"
+        columns={columns}
+        dataSource={classes}
+        size="middle"
+        className="table-middle"
+        pagination={false}
+      />
+      {/*{isUserAuth && (*/}
+      <div className="d-flex justify-content-end my-5">
+        <ClassesDashboardControls />
+      </div>
+      {/*)}*/}
+    </div>
+  );
+};
+
+const mapStateToProps = (state: any) => ({
+  Account: state.Account,
+  sidepanel: state.Sidepanel,
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  open: (payload: ISidepanel) => dispatch({ type: 'Sidepanel/open', payload }),
+
+});
+
+export default withRouter(TeacherAccountViewClassesList);
