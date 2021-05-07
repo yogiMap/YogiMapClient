e08a864062744371df3c9c4f70303c934a5ac0ec
gayfulina <irinmdr@gmail.com>
2021-05-06 17:09:08 -0700

    burger2

--- src/layout/BurgerMenu.tsx
+++ src/layout/BurgerMenu.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'umi';
 import logo from '@/icons/logo_yogimap.svg';
-import logoSmall from '@/icons/logo_yogymap_small.svg'
+import logoSmall from '@/icons/logo_yogymap_small.svg';
 import burger from '@/icons/burger.svg';
 import UserInfo from '@/pages/user/topInfo/UserInfo';
 import AdminMenu from '@/layout/_menu/AdminMenu';
@@ -16,74 +16,29 @@ const BurgerMenu = () => {
 
   return (
     <div>
+      <div className="burger-nav">
+        <div className="burger">
+          <button onClick={toggleOpenBurgerMenu}>
+            <img src={burger} alt="click me" height={35} />
+          </button>
+        </div>
 
-
-      <div className="main-nav">
-      <nav className="main-nav__items">
-
-        <Link to="/" className="site-name text-nowrap">
-          <img src={logo} alt="Yogi Map Logo" height={40} className="logo mr-2" />
+        <Link to="/" className="logo-burger">
+          <img src={logoSmall} alt="Yogi Map LogoSmall" height={35} />
         </Link>
-
-        <div className="main-nav__item">
-          <TopMenu />
-        </div>
-
-        <div className="main-nav__item">
-          <AdminMenu />
-        </div>
-
-        <div className="main-nav__item">
-          <UserInfo />
-        </div>
-      </nav>
+        {openBurgerMenu && (
+          <>
+            <nav className="mobile-nav">
+              {/*<div className="mobile-user-info">*/}
+              {/*  <UserInfo />*/}
+              {/*</div>*/}
+              <div className="mobile-nav__items">
+                <TopMenu />
+              </div>
+            </nav>
+          </>
+        )}
       </div>
-
-
-
-<div className="burger-nav">
-      <div className="burger">
-        <button onClick={toggleOpenBurgerMenu}>
-          <img src={burger} alt="click me" height={35} />
-        </button>
-      </div>
-
-      <Link to="/" className="logo-burger">
-        <img src={logoSmall} alt="Yogi Map LogoSmall" height={35}  />
-      </Link>
-      {openBurgerMenu && (
-        <>
-          <nav className="mobile-nav">
-            <div className="mobile-user-info">
-              <UserInfo />
-            </div>
-            <div className="mobile-nav__items">
-              <TopMenu />
-            </div>
-          </nav>
-        </>
-      )}
-</div>
-
-      {/*<header className="main-header">*/}
-      {/*  <div>*/}
-      {/*    <button>*/}
-      {/*      <img src={burger} alt="click me" height={40} />*/}
-      {/*    </button>*/}
-
-          {/*<Link to="/" className="site-name text-nowrap">*/}
-          {/*  <img src={logoSmall} alt="Yogi Map Logo" height={40} className="logo mr-2" />*/}
-          {/*</Link>*/}
-        {/*</div>*/}
-
-      {/*  <nav className="main-nav">*/}
-      {/*    <TopMenu />*/}
-      {/*  </nav>*/}
-      {/*</header>*/}
-
-      {/*  <nav className="mobile-nav">*/}
-      {/*    <TopMenu />*/}
-      {/*  </nav>*/}
     </div>
   );
 };
--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -8,57 +8,29 @@ import BurgerMenu from '@/layout/BurgerMenu';
 
 const Navbar = () => {
   return (
-    // <nav className='navbar navbar-expand-lg navbar-light bg-light-'>
-    //   <div className='container-fluid'>
-    //        <Link to="/" className="site-name text-nowrap">
-    //        <img src={logo} alt="Yogi Map Logo" height={40} className="logo mr-2" />
-    //       </Link>
-    //
-    //     {/*<button className='custom-toggler navbar-toggler'*/}
-    //     {/*        type='button'*/}
-    //     {/*        data-toggle='collapse'*/}
-    //     {/*        data-target='#navbarsExample09'*/}
-    //     {/*        aria-controls='navbarsExample09'*/}
-    //     {/*        aria-expanded={!isNavCollapsed ? true : false}*/}
-    //     {/*        aria-label='Toggle navigation'*/}
-    //     {/*        onClick={handleNavCollapse}>*/}
-    //     {/*  <span className='navbar-toggler-icon'></span>*/}
-    //     {/*</button>*/}
-    //
-    //         <AdminMenu />
-    //         <TopMenu />
-    //         <UserInfo />
-    //
-    //   </div>
-    // </nav>
+    <div>
+      <div className="main-nav">
+        <nav className="main-nav__items">
+          <Link to="/" className="site-name text-nowrap">
+            <img src={logo} alt="Yogi Map Logo" height={40} className="logo mr-2" />
+          </Link>
 
-    // <nav className="navbar navbar-expand-xl">
-    //   <div className="container flex-nowrap">
-    //     <Link to="/" className="site-name text-nowrap">
-    //       <img src={logo} alt="Yogi Map Logo" height={40} className="logo mr-2" />
-    //     </Link>
-    //
-    //     <div className="navbar-toggler">
-    //       <div className="d-flex align-items-center">
-    //         <UserInfo />
-    //         <TopMenuDropdown />
-    //         <UserInfoNavCollapse />
-    //       </div>
-    //     </div>
-    //
-    //     <div className="collapse navbar-collapse">
-    //       <div className="navbar-nav">
-    //         <TopMenu />
-    //         <AdminMenu />
-    //       </div>
-    //
-    //
-    //     </div>
-    //   </div>
-    // </nav>
+          <div className="main-nav__item">
+            <TopMenu />
+          </div>
 
-    <BurgerMenu/>
+          <div className="main-nav__item">
+            <AdminMenu />
+          </div>
 
+          <div className="main-nav__item">
+            <UserInfo />
+          </div>
+        </nav>
+      </div>
+
+      <BurgerMenu />
+    </div>
   );
 };
 
