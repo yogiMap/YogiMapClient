f11a8bd4f7a420592c1219457a78c17f7ce8ce4f
gayfulina <irinmdr@gmail.com>
2021-05-26 19:45:08 -0700

    account

--- src/pages/teacherAccount/form/TeacherAccountForm.tsx
+++ src/pages/teacherAccount/form/TeacherAccountForm.tsx
@@ -10,13 +10,10 @@ import FocusSearchInput from '@/pages/utils/searchInput/FocusSearchInput';
 import { useForm } from 'antd/es/form/Form';
 import { connect, withRouter } from 'umi';
 import { IUserAccount } from '@/pages/user/userSearch/types';
-import JsApiLoaderOpts from '@/pages/utils/googleUt/GoogleMap/JsApiLoaderOpts';
-import { useJsApiLoader } from '@react-google-maps/api';
 import { IClassType } from '@/pages/classType/types';
 import { IClasses } from '@/pages/classes/types';
 import { IEvent } from '@/pages/event/types';
 import { IStyle } from '@/pages/style/types';
-import Autocomplete from '@/pages/utils/googleUt/GoogleMap/Autocomplete';
 import validator from '@/utils/validators';
 import TeacherAccountDashboardControlsDelete from '@/pages/user/settings/teacherAccount/controls/TeacherAccountDashboardControlsDelete';
 
@@ -39,23 +36,13 @@ const TeacherAccountForm = (props: IProps) => {
   const name = get(props, 'Account.name', '');
   const email = get(props, 'Account.email', '');
 
-  // const [addAdditionalPhoneMode, setAddAdditionalPhoneMode] = useState(false);
   const [form] = useForm();
-  // const opts = JsApiLoaderOpts();
-  // // @ts-ignore
-  // const { isLoaded, loadError } = useJsApiLoader(opts);
-  //
-  // const [addressFields, setAddressFields] = useState();
-  //
-  // const onChange = (center: any) => {
-  //   center ? setAddressFields(center[1]) : null;
-  // };
-  // useEffect(() => form.setFieldsValue(addressFields));
 
   console.log(props.styleList, '*******************');
   // const style = get(props, 'styleList', []);
   if (!props.styleList) return null;
 
+  // @ts-ignore
   return (
     <div className="container mt-5">
       <Form onFinish={props.onFinish} initialValues={props.initialValues} layout="vertical" name="teacherAccount">
@@ -83,19 +70,13 @@ const TeacherAccountForm = (props: IProps) => {
           </div>
 
           <div className="col-md-6">
-            <Form.Item name="description" label="Description">
-              <Input.TextArea autoSize={{ minRows: 2, maxRows: 4 }} className="rounded-pill" />
+            <Form.Item label="Yoga Focus" name="focus">
+              <FocusSearchInput />
             </Form.Item>
           </div>
         </div>
 
         <div className="row">
-          <div className="col-md-6">
-            <Form.Item label="Yoga Focus" name="focus">
-              <FocusSearchInput />
-            </Form.Item>
-          </div>
-
           <div className="col-md-6">
             <Form.Item name="styleList" label="Yoga Style">
               <Select>
@@ -127,6 +108,12 @@ const TeacherAccountForm = (props: IProps) => {
               <PhoneInput label="Phone" name="phoneNumber" required={true} form={form} />
             </Form.Item>
           </div>
+
+          <div className="col-md-6">
+            <Form.Item name="description" label="Description">
+              <Input.TextArea autoSize={{ minRows: 2, maxRows: 4 }} className="rounded-pill" />
+            </Form.Item>
+          </div>
         </div>
 
         <div className="row">
@@ -136,16 +123,6 @@ const TeacherAccountForm = (props: IProps) => {
         </div>
 
         <div className="row">
-          <div className="col-md-10">
-            {/*{isLoaded ? (*/}
-            {/*  <Form.Item label="Address Search">*/}
-            {/*    <Autocomplete onChange={onChange} />*/}
-            {/*  </Form.Item>*/}
-            {/*) : (*/}
-            {/*  loadError*/}
-            {/*)}*/}
-          </div>
-
           <div className="col-md-2 pt-4">
             <Button type="primary" shape="round" className="mt-2">
               Search
@@ -213,7 +190,9 @@ const TeacherAccountForm = (props: IProps) => {
           </div>
 
           <div className="col">
-            <Form.Item className="mx-3">{/*<TeacherAccountDashboardControlsDelete />*/}</Form.Item>
+            <Form.Item className="mx-3">
+              <TeacherAccountDashboardControlsDelete />{' '}
+            </Form.Item>
           </div>
         </div>
       </Form>
