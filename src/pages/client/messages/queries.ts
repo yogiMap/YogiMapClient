b77a10146356132b5384823da9e1801e2d92f79f
gayfulina <irinmdr@gmail.com>
2021-06-20 18:34:04 -0700

    refactor

--- src/pages/client/messages/queries.ts
+++ src/pages/client/messages/queries.ts
@@ -1,10 +1,10 @@
 import { get, patch, post, del } from '@/utils/httpMethods';
 
 export async function queryClientMessageSend(payload: any): Promise<any> {
-  return post({ url: '/message/send-sms', data: payload });
+  return post({ url: '/telephony/message/send-sms', data: payload });
 }
 
 export async function queryClientMessageSearch(payload: any): Promise<any> {
   console.log('MESSAGES SEARCH PAYLOAD: ', payload);
-  return post({ url: '/message/search', data: payload });
+  return post({ url: '/telephony/message/search', data: payload });
 }
