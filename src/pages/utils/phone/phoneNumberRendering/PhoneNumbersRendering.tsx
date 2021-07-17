dec33135d1ac1437c407c0af5d0d8430f867549b
gayfulina <irinmdr@gmail.com>
2021-07-17 12:59:07 -0700

    twilio calls3

--- src/pages/utils/phone/phoneNumberRendering/PhoneNumbersRendering.tsx
+++ src/pages/utils/phone/phoneNumberRendering/PhoneNumbersRendering.tsx
@@ -1,17 +1,52 @@
 import React from 'react';
 import { get } from 'lodash';
+import callOutbound from '@/icons/phone-call.svg';
+import { Button } from 'antd';
+import { connect } from 'umi';
+import { ISidepanel } from '@/pages/utils/sidepanel/types';
 
-function RenderPhoneNumber(props: any) {
+export interface IProps {
+  onClick: () => void;
+  phoneNumberAll: string;
+  open: (arg: any) => void;
+}
+
+function RenderPhoneNumber(props: IProps) {
   const phoneNumberAll = props.phoneNumberAll;
   const phoneNumberCode = get(phoneNumberAll, 'code', '1');
   const phoneNumber = get(phoneNumberAll, 'number');
+  const phoneNumberExt = get(phoneNumberAll, 'ext');
   let formattedPhone = '';
   if (phoneNumber && phoneNumber.length) {
     const areaCode = phoneNumber.slice(0, 3);
     const phonePartOne = phoneNumber.slice(3, 6);
     const phonePartTwo = phoneNumber.slice(6);
-    formattedPhone = ` +${phoneNumberCode} (${areaCode}) ${phonePartOne}-${phonePartTwo}`;
+
+    formattedPhone = ` +${phoneNumberCode} (${areaCode}) ${phonePartOne}-${phonePartTwo}  `;
   }
-  return <>{phoneNumber && <span>{formattedPhone}</span>}</>;
+
+  const onClickHandler = () => {
+    props.open({
+      title: 'Calls',
+      component: 'CallsPanel',
+      place: '',
+      width: 380,
+    });
+  };
+
+  return (
+    <>
+      {phoneNumber && <span>{formattedPhone} </span>}
+      {phoneNumberExt && <span>ext {phoneNumberExt}</span>}
+      <Button type="link" onClick={onClickHandler}>
+        {<img src={callOutbound} alt="" height="20" />}
+      </Button>
+    </>
+  );
 }
-export default RenderPhoneNumber;
+
+const mapDispatchToProps = (dispatch: any) => ({
+  open: (payload: ISidepanel) => dispatch({ type: 'Sidepanel/open', payload }),
+});
+
+export default connect(null, mapDispatchToProps)(RenderPhoneNumber);
